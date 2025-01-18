e1301e655e9af46d8929c6751ac938d4ce7f9348
Your Name <you@example.com>
2025-01-18 21:25:03 +0800

    add list orders by month

--- controllers/order.controller.go
+++ controllers/order.controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -35,22 +36,67 @@ func (ctrl *OrderController) CreateOrder(ctx *gin.Context) {
 }
 
 // GetOrders retrieves all orders
+// func (ctrl *OrderController) GetOrders(ctx *gin.Context) {
+// 	var orders []models.Order
+// 	if err := ctrl.DB.Preload("Contractor").
+// 		Preload("Driver").
+// 		Preload("Truck").
+// 		Order("updated_at DESC").
+// 		Find(&orders).Error; err != nil {
+// 		ctx.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to retrieve orders"})
+// 		return
+// 	}
+
+// 	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": orders})
+// }
+
 func (ctrl *OrderController) GetOrders(ctx *gin.Context) {
+	month := ctx.Query("month")
+	year := ctx.Query("year")
+
+	// validate the parameters
+	if month == "" || year == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "error", "message": "Month and Year are required"})
+		return
+	}
+
+	// convert month and year to integer
+
+	monthInt, err := strconv.Atoi(month)
+	if err != nil || monthInt < 1 || monthInt > 12 {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": "Invalid month"})
+		return
+	}
+
+	yearInt, err := strconv.Atoi(year)
+	if err != nil || yearInt < 2018 || yearInt > 2100 {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": "Invalid year"})
+		return
+	}
+
+	// query database with specific month and year
 	var orders []models.Order
 	if err := ctrl.DB.Preload("Contractor").
 		Preload("Driver").
 		Preload("Truck").
+		Where("EXTRACT(MONTH from updated_at) = ? AND EXTRACT(YEAR FROM updated_at) = ?", monthInt, yearInt).
+		Order("updated_at DESC").
 		Find(&orders).Error; err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"status": "error", "message": "Failed to retrieve orders"})
 		return
 	}
-
+	// Return the orders as a JSON response
 	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": orders})
 }
 
 // GetOrder retrieves a specific order by ID
 func (ctrl *OrderController) GetOrderByID(c *gin.Context) {
-	id := c.Param("id")
+	id := c.Param("orderId")
+	if _, err := uuid.Parse(id); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Invalid order ID format"})
+		return
+	}
+
 	var order models.Order
 	if err := ctrl.DB.Preload("Contractor").
 		Preload("Driver").
@@ -69,8 +115,13 @@ func (ctrl *OrderController) GetOrderByID(c *gin.Context) {
 
 // UpdateOrder updates an existing order
 func (ctrl *OrderController) UpdateOrder(c *gin.Context) {
-	id := c.Param("id")
+	id := c.Param("orderId")
 	var order models.Order
+
+	if _, err := uuid.Parse(id); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Invalid order ID format"})
+		return
+	}
 
 	if err := ctrl.DB.First(&order, "id = ?", id).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
